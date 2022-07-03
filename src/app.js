657d22eab3a093cbd4a58791be960f75b5d94238
peace <peace9917@yahoo.com>
2022-07-03 01:58:34 +0100

    added spinner and implemented the api callBacks

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ weatherApp.config(function($routeProvider){
         controller: 'forecastController'
     })
 
+    .when('/forecast/:days',{
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+
 });
 
 // SERVICES
@@ -36,8 +41,40 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('forecastController', ['$scope', '$http', '$resource', '$routeParams', 'cityService', 
+    function($scope, $http, $resource, $routeParams, cityService){
 
-    $scope.city = cityService.city;
+    const apiURL = 'http://api.weatherstack.com/current?access_key=dae43f3d982a36f37112aa92b1b522b4&query=';
+
+    $scope.spinner = true;
+
+    var city = cityService.city ?? 'Lagos';
+
+    $http.get(getURL())
+        .then(function (response) {
+            var location = response.data.location;
+            var current = response.data.current;
+
+            $scope.country = location.country;
+            $scope.region = location.region;
+            $scope.timezone = location.timezone_id;
+            $scope.localtime = location.localtime;
+
+            $scope.weather_icons = current.weather_icons;
+            $scope.temperature = current.temperature;
+            $scope.weather_descriptions = current.weather_descriptions[0];
+            $scope.is_day = current.is_day === 'yes' ? true : false;
+
+            $scope.spinner = false;
+
+        });
+
+    function getURL(){
+        if(city){
+            return apiURL + city;
+        }else{
+            return apiURL + 'Lagos';
+        }
+    }
 
 }]);
